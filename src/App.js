ba2f97d45420d6ca700c61b743a3ff568f6c915e
Mark Joshua Garcia <112916162+Markkk21@users.noreply.github.com>
2024-05-07 14:47:17 +0800

    Added Dashboard Page

--- src/App.js
+++ src/App.js
@@ -4,27 +4,20 @@ import { Box } from '@material-ui/core';
 import logo from './logo.svg';
 import './App.css';
 
+import Dashboard from './Components/Pages/Dashboard';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Box>        
-            <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <Box> 
+          <Dashboard />
+        
         </Box>
       </header>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Components/Pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Tline from '../Layout/Timeline';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>This is Dashboard</h1>
+
+      <Tline/>
+    </div>
+  );
+}
+
+export default Dashboard;
