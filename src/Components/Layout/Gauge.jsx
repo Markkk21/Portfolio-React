6c1b6db0f770c690d0ff6107e319cdf9dc2c7509
Mark Joshua Garcia <112916162+Markkk21@users.noreply.github.com>
2024-06-06 14:26:59 +0800

    Updated TechSkill, Added Animation

---
+++ src/Components/Layout/Gauge.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Box, CircularProgress, Typography } from '@mui/material';
+
+function CustomGauge({ value, label }) {
+  return (
+    <Box position="relative" display="inline-flex">
+      <CircularProgress variant="determinate" value={value} size={100} />
+      <Box
+        top={0}
+        left={0}
+        bottom={0}
+        right={0}
+        position="absolute"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Typography variant="caption" component="div">
+          {`${Math.round(value)}%`}
+        </Typography>
+      </Box>
+      {label && (
+        <Box mt={1}>
+          <Typography variant="body2">
+            {label}
+          </Typography>
+        </Box>
+      )}
+    </Box>
+  );
+}
+
+export default CustomGauge;
--- src/Components/Pages/About/TechSkill.jsx
+++ src/Components/Pages/About/TechSkill.jsx
@@ -16,23 +16,23 @@ import {
 } from "react-icons/si";
 import { TbBrandReactNative } from "react-icons/tb";
 import useIntersectionObserver from '../../Layout/Transition';
-// import './TechSkill.css'; // Import the CSS file
+import CustomGauge from '../../Layout/Gauge';
 
 function TechSkill() {
   const observerRef = useIntersectionObserver('visible');
 
   const techSkills = [
-    { icon: <DiHtml5 />, key: "html5" },
-    { icon: <DiCss3 />, key: "css3" },
-    { icon: <DiJavascript1 />, key: "js" },
-    { icon: <DiReact />, key: "react" },
-    { icon: <SiMui />, key: "mui" },
-    { icon: <SiPhp />, key: "php" },
-    { icon: <SiLaravel />, key: "laravel" },
-    { icon: <DiPython />, key: "python" },
-    { icon: <TbBrandReactNative />, key: "react-native" },
-    { icon: <SiBootstrap />, key: "bootstrap" },
-    { icon: <DiJava />, key: "java" },
+    { icon: <DiHtml5 />, key: "html5", value: 80 },
+    { icon: <DiCss3 />, key: "css3", value: 70 },
+    { icon: <DiJavascript1 />, key: "js", value: 90 },
+    { icon: <DiReact />, key: "react", value: 85 },
+    { icon: <SiMui />, key: "mui", value: 60 },
+    { icon: <SiPhp />, key: "php", value: 50 },
+    { icon: <SiLaravel />, key: "laravel", value: 55 },
+    { icon: <DiPython />, key: "python", value: 75 },
+    { icon: <TbBrandReactNative />, key: "react-native", value: 65 },
+    { icon: <SiBootstrap />, key: "bootstrap", value: 70 },
+    { icon: <DiJava />, key: "java", value: 60 },
   ];
 
   useEffect(() => {
@@ -54,7 +54,7 @@ function TechSkill() {
                 {skill.icon}
               </div>
               <div className="flip-card-back">
-                {skill.key}
+                <CustomGauge value={skill.value} />
               </div>
             </div>
           </div>
